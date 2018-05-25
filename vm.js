5327183c144b2848875cf9e93065c4db94306cc5
daidai-gtlf <gootalife0512@gmail.com>
2018-05-25 14:31:40 +0900

    出力機能

--- vm.js
+++ vm.js
@@ -15,13 +15,36 @@ var info = new Vue({
 var attendance = new Vue({
     el: '#attendanceModel',
     data: {
-        TR: '',
-        M2: '',
-        M1: '',
-        B4: '',
-        B3: '',
-        B2: '',
-        B1: ''
+        attendances: [
+            {
+                grade: 'TR',
+                members: ''
+            },
+            {
+                grade: 'M2',
+                members: ''
+            },
+            {
+                grade: 'M1',
+                members: ''
+            },
+            {
+                grade: 'B4',
+                members: ''
+            }, 
+            {
+                grade: 'B3',
+                members: ''
+            },
+            {
+                grade: 'B2',
+                members: ''
+            },
+            {
+                grade: 'B1',
+                members: ''
+            },
+        ]
     }
 });
 
@@ -169,11 +192,69 @@ var result = new Vue({
         schedule: schedule,
         extraSchedule: extraSchedule,
         note: note,
+        title: '',
         outputText: ''
     },
     methods: {
         generateProceeding() {
-            this.outputText = `${this.info.stuNum}`;
+            this.title = `${this.info.stuNum} [SLP] 定例活動 ${this.info.date}`
+
+            let str = `□ ${this.info.stuNum} [SLP] 定例活動 ${this.info.date} ${this.info.stuName}\n\n`
+            + '★ 次第\n\n● 出席\n\n';
+            let block = '';
+            for (let index in this.attendance.attendances) {
+                if (this.attendance.attendances[index].members !== '') {
+                    block += `○ ${this.attendance.attendances[index].grade} ${this.attendance.attendances[index].members}\n`
+                }
+            }
+            if (block !== '') {
+                str += block + '\n';
+            }
+
+            str += `○ 議事 ${this.info.stuName}\n\n`
+            + `○ 次回議事 ${this.info.nextStuName}\n\n`;
+            
+            str += '● 進行\n\n';
+            for (let index in this.process.processes) {
+                str += `○ ${this.process.processes[index].time} ${this.process.processes[index].overview}\n`;
+                if (index == this.process.processes.length - 1) {
+                    str += '\n';
+                }
+            }
+
+            for (let index in this.contact.contacts) {
+                str += '★ 事務連絡\n\n';
+                str += `● ${this.contact.contacts[index].overview}\n\n${this.contact.contacts[index].detail}\n\n`;
+            }
+
+            str += '★ 活動\n\n';
+            for (let index in this.activity.activities) {
+                str += `● ${this.activity.activities[index].overview}\n\n${this.activity.activities[index].detail}\n\n`;
+            }
+
+            str += '■ 予定\n\n';
+            for (let index in this.schedule.schedules) {
+                str += `○ ${this.schedule.schedules[index].date} ${this.schedule.schedules[index].time} ${this.schedule.schedules[index].overview}\n`;
+                if (index == this.schedule.schedules.length - 1) {
+                    str += '\n';
+                }
+            }
+            
+            for (let index in this.extraSchedule.extraSchedules) {
+                str += '■ 課外\n\n';
+                str += `○ ${this.extraSchedule.extraSchedules[index].date} ${this.extraSchedule.extraSchedules[index].time} ${this.extraSchedule.extraSchedules[index].overview}\n`;
+                if (index == this.extraSchedule.extraSchedules.length - 1) {
+                    str += '\n';
+                }
+            }
+
+            if (this.note.content !== '') {
+                str += '■ 備考\n\n'
+                + this.note.content;
+            }
+            
+            
+            this.outputText = str;
         }
     }
 });
